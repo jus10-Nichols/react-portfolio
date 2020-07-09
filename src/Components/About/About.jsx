a811946ea239e17d33d0c16d71e1b772a69b25ff
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-07-09 12:30:09 -0500

    Morning: Formatted Footer/ added about section place holder and image.
    TODO today: add content for about/bio set up links to Github/Linkedin,email. Add resume. Projects Section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import './About.css'
+import './About.css';
+import ProfilePic from '../../assets/profilePic.png';
+
 export default function About(){
     return(
         <div className ="aboutMe">
             <div className = "introBody">
-            <h1>About Me</h1>
+            <div className = "profilePic">
+            <img  alt= "myFace" class="circleImg" src={ProfilePic} />
             </div>
-            
+            <div className = "myBio">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus aperiam, autem facilis sed odio non magnam incidunt optio voluptatem quis, id sint ipsum. Consectetur veniam quos 
+                    voluptatem laudantium quibusdam!</p>
+            </div>
+            </div>
         </div>
     )
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import Typed from "react-typed";
+import './Footer.css'
 export default function Footer(){
     return(
         <div className ='footer'>
-            <h1>Footer</h1>
+            <div className = "container">
+
+        <div className ="myName">
+                <p><b><Typed
+            strings={[
+            "Full-Stack Developer",
+            "Front-End Developer",
+            "Back-End Developer",
+            "Software Engineer",
+            "Web Developer",
+        ]}
+        typeSpeed={33}
+        backSpeed={60}
+        loop
+        /></b></p>
+        </div>
+                <div className = "myLocation">
+                <p>📍 Living in <b>Minneapolis, MN.</b></p>
+                </div>
+                <div className ="contactMe">
+                <p><b>YOU CAN REACH ME AT:</b></p>
+			<p>justin.g.nichols.93@gmail.com</p>
+			<p>(952) 270-9736</p>
+                </div>
+            </div>
         </div>
     )
 }
