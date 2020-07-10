7b46957583047f86532cac29b9a15538594d9593
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-07-10 10:26:48 -0500

    Update Github.jsx

--- src/Components/Github/Github.jsx
+++ src/Components/Github/Github.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import axios from 'axios';
-import GithubCard from './GithubCard';
-import {Link} from "@material-ui/core";
-import './Github.css'
+import React from "react";
+import axios from "axios";
+import GithubCard from "./GithubCard";
+import { Link } from "@material-ui/core";
+import "./Github.css";
 
 const aStyle = {
-    color: '#16a085',
+  color: "#16a085",
 };
 
 export default class GithubInfo extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-        user: {},
-        followers:[]
-        };
-    }
-    componentDidMount() {
-        axios
-        .get("https://api.github.com/users/jnichols93")
-        .then(res => this.setState({ user: res.data }))
-        //.catch(err => console.log("error"));
-        axios
-        .get("https://api.github.com/users/jnichols93/followers")
-        
-        .then(res => this.setState({ followers: res.data }))}
-        
-        render() {
-            return (
-                <div className = 'gitProfile'>
-                    <Link style={aStyle} href='https://github.com/jnichols93'>
-                    <GithubCard user = {this.state.user}/>
-                    </Link>
-                </div>
-            );}
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      followers: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/jnichols93")
+      .then((res) => this.setState({ user: res.data }))
+      .catch((err) => console.log("error"));
+    axios
+      .get("https://api.github.com/users/jnichols93/followers")
 
+      .then((res) => this.setState({ followers: res.data }));
+  }
 
+  render() {
+    return (
+      <div className="gitProfile">
+        <Link style={aStyle} href="https://github.com/jnichols93">
+          <GithubCard user={this.state.user} />
+        </Link>
+      </div>
+    );
+  }
 }
