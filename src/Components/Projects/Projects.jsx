af4eb928b2db5c633d3d02e3e12c277cc05e92cd
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-07-12 14:26:46 -0500

    Droom finished

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -84,7 +84,7 @@ export default function Projects(){
       <div className = "wdInfo">
 
       <div className = 'wdImg'>
-      <img  alt= "Food Truck" className="prjctImg" src={Droom}/>
+      <img  alt= "droom" className="prjctImg" src={Droom}/>
       <div className ='wdLinks'>
         <div className = 'deployed'>
 
@@ -96,17 +96,20 @@ export default function Projects(){
       </div>
         
         <div className = 'wdBio'>
-        <h4>Well-Done</h4>
-				<p>Well-Done Is an IoT application designed to assist Well-Done in their mission, by persisting, storing, and displaying the data collected by their Mo-Mo sensors. To provide decision-makers with a tool to see which pumps are down and need maintenance, if the well is running low, and detailed status reports updated hourly for each pump. Our team started working in early December and finished mid-February. However, in April, I and a few others were contacted by Well-Done directly and asked if we could continue on a volunteer basis! We've come a long way since then, and are almost ready for roll-out.  </p>
+        <h4>Droom</h4>
+        <p>Droom lets job seekers swipe right on their dream job! 
+			Job posters simply post their job opening on Droom, 
+			and qualified candidates can swipe right to confirm their interest! As a build-week project, I was given four days to produce a fully functional react/redux front-end.</p>
 				<h4>Tech Stack:</h4> 
-        <p>React, Redux, ag-Grid, SASS, Semantic UI, Reactstrap, NodeJS, PostgreSQL, Express, SQL, Knex.</p>
+        <p>React, Redux, Material-UI, CSS, SASS</p>
 				<h4>My Contributions:</h4>
 				<ul>
-				<li>Created an activity calendar that uses color to indicate the status by day for an individual sensor and water pump.</li> 
-				<li>Used ag-grid to build a grid to display all of the organizations currently using Well-Done's devices in pumps they are maintaining or have installed.</li>
-				<li>Implemented foreign keys on an existing, active database to enhance API functionality. </li>
-				<li>Added the ability to filter and export pump data to an excel spread sheet.</li>
-				<li>Created a global mix-ins SASS stylesheet with all colors needed, and common component stylings according to our Figma to make styling changes quick and easy.</li>
+				<li>Figured out how to make react swipe right.</li>
+			<li>Implemented React/Redux</li>
+			<li>Learned Material-Ui</li>
+			<li>Learned React-Spring</li>
+			<li>Implemented API calls</li>
+			<li>Built Stateful Components</li>
         </ul>
       </div>
       </div>
