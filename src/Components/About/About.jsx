997b40cd57e653931534b6cab922a0b4c1389c91
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-09-09 15:50:53 -0500

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -23,7 +23,7 @@ export default function About(){
                 I started learning HTML and CSS while in HighSchool.However, I enlisted in the United States Marine Corps after graduation and put my fascination with web development (temporarily) aside. My military service took me all over the world! I participated in several Humanitarian aid operations, joint training exercises, and 
                 even did a stint as the personal radio operator for the commanding General of the Third Marine Division.</p>
                 <p>After my five years were up, I decided to attend college at the University of Houston. I wanted to pursue a degree in computer science, and a career in Software/Web Development. Unfortunately, no plan survives first contact. Due to financial needs, and admittedly a rough time trying to re-enter the civilian world I withdrew and focused on work full time.
-                In 2019 while working as regional sales manager (MN, ND, SD, IA, WI) for a local home remodeling company when I came across Lambda School. A nine month immersive learning program focused on full-stack Web Development, Computer, and Data Science. Having lost my passion for sales, I made a decision. I wanted to get back in the saddle, get into web development again, and change careers.</p>
+                In 2019 while working as regional sales manager (MN, ND, SD, IA, WI) for a local home remodeling company I came across Lambda School. A nine month immersive learning program focused on full-stack Web Development, Computer, and Data Science. Having lost my passion for sales, I made a decision. I wanted to get back in the saddle, get into web development again, and change careers.</p>
                 </div>
             </div>
             </div>
